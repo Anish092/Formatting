3dda9a425fe9a219bdae762f5f8aea46f44e5c42
Anish092 <37304018+Anish092@users.noreply.github.com>
2018-09-10 09:31:00 -0400

    first

---
+++ Formatting.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Formatting
+{
+	    public static void main(String[] args)
+	    {
+		System.out.printf("%50s", "Number of items purchased:");
+		System.out.printf("%24d", 15);
+		System.out.println();
+		System.out.printf("%50s", "Subtotal:");
+		System.out.printf("%24s", "142.18");
+		System.out.println();
+		System.out.printf("%50s", "Tax:");
+		System.out.printf("%24s", "5.66");
+		System.out.println();
+	    System.out.printf("%50s", " ");
+		System.out.printf("%24s", "------" );
+		System.out.println();
+		System.out.printf("%50s", "Total:");
+		System.out.printf("%24s", "$147.84");
+		System.out.println();
+		System.out.println();
+		System.out.printf("%50s", "Gas points earned:");
+		System.out.printf("%24s", "100");
+		System.out.println();
+
+
+
+
+
+
+
+	}
+
+}
